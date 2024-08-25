38c09fbe066d6633fbd49305772abfe7d80fae63
Sem Van Broekhoven <sem.van.broekhoven@gmail.com>
2024-08-25 15:23:49 +0200

    first test setup

--- lib/screens/tap_tap_counter_screen.dart
+++ lib/screens/tap_tap_counter_screen.dart
@@ -1,7 +1,5 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:tap_tap_money_counter/screens/widgets/money_button.dart';
 
 class TapTapCounterScreen extends StatefulWidget {
   const TapTapCounterScreen({super.key});
@@ -11,6 +9,8 @@ class TapTapCounterScreen extends StatefulWidget {
 }
 
 class _TapTapCounterScreenState extends State<TapTapCounterScreen> {
+  List<double> moneySequence = [0];
+  double moneyCounter = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,17 +19,79 @@ class _TapTapCounterScreenState extends State<TapTapCounterScreen> {
           Expanded(
             child: Row(
               children: [
-                Expanded(
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    child: Image.asset("assets/euro/0,01.png"),
-                  ),
+                MoneyButton(
+                  value: 0.01,
+                  onPressed: (value) {
+                    setState(() {
+                      moneyCounter += value;
+                      moneySequence.add(value);
+                    });
+                  },
+                  imagePath: ("assets/euro/0,01.png"),
                 ),
-                Expanded(
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    child: Image.asset("assets/euro/0,02.png"),
-                  ),
+                MoneyButton(
+                  value: 0.02,
+                  onPressed: (value) {
+                    setState(() {
+                      moneyCounter += value;
+                      moneySequence.add(value);
+                    });
+                  },
+                  imagePath: ("assets/euro/0,02.png"),
+                ),
+                MoneyButton(
+                  value: 0.05,
+                  onPressed: (value) {
+                    setState(() {
+                      moneyCounter += value;
+                      moneySequence.add(value);
+                    });
+                  },
+                  imagePath: ("assets/euro/0,05.png"),
+                ),
+              ],
+            ),
+          ),
+          // Expanded(
+          //   child: Row(
+          //     children: [
+                
+                
+          //     ],
+          //   ),
+          // ),
+          Expanded(
+            child: Row(
+              children: [
+                MoneyButton(
+                  value: 0.10,
+                  onPressed: (value) {
+                    setState(() {
+                      moneyCounter += value;
+                      moneySequence.add(value);
+                    });
+                  },
+                  imagePath: ("assets/euro/0,1.png"),
+                ),
+                MoneyButton(
+                  value: 0.20,
+                  onPressed: (value) {
+                    setState(() {
+                      moneyCounter += value;
+                      moneySequence.add(value);
+                    });
+                  },
+                  imagePath: ("assets/euro/0,2.png"),
+                ),
+                MoneyButton(
+                  value: 0.50,
+                  onPressed: (value) {
+                    setState(() {
+                      moneyCounter += value;
+                      moneySequence.add(value);
+                    });
+                  },
+                  imagePath: ("assets/euro/0,5.png"),
                 ),
               ],
             ),
@@ -37,35 +99,25 @@ class _TapTapCounterScreenState extends State<TapTapCounterScreen> {
           Expanded(
             child: Row(
               children: [
-                Expanded(
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    child: Image.asset("assets/euro/0,05.png"),
-                  ),
+                MoneyButton(
+                  value: 1,
+                  onPressed: (value) {
+                    setState(() {
+                      moneyCounter += value;
+                      moneySequence.add(value);
+                    });
+                  },
+                  imagePath: ("assets/euro/1.png"),
                 ),
-                Expanded(
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    child: Image.asset("assets/euro/0,1.png"),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Expanded(
-            child: Row(
-              children: [
-                Expanded(
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    child: Image.asset("assets/euro/0,2.png"),
-                  ),
-                ),
-                Expanded(
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    child: Image.asset("assets/euro/0,5.png"),
-                  ),
+                MoneyButton(
+                  value: 2,
+                  onPressed: (value) {
+                    setState(() {
+                      moneyCounter += value;
+                      moneySequence.add(value);
+                    });
+                  },
+                  imagePath: ("assets/euro/2.png"),
                 ),
               ],
             ),
@@ -73,35 +125,25 @@ class _TapTapCounterScreenState extends State<TapTapCounterScreen> {
           Expanded(
             child: Row(
               children: [
-                Expanded(
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    child: Image.asset("assets/euro/1.png"),
-                  ),
+                MoneyButton(
+                  value: 5,
+                  onPressed: (value) {
+                    setState(() {
+                      moneyCounter += value;
+                      moneySequence.add(value);
+                    });
+                  },
+                  imagePath: ("assets/euro/5.png"),
                 ),
-                Expanded(
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    child: Image.asset("assets/euro/2.png"),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Expanded(
-            child: Row(
-              children: [
-                Expanded(
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    child: Image.asset("assets/euro/5.png"),
-                  ),
-                ),
-                Expanded(
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    child: Image.asset("assets/euro/10.png"),
-                  ),
+                MoneyButton(
+                  value: 10,
+                  onPressed: (value) {
+                    setState(() {
+                      moneyCounter += value;
+                      moneySequence.add(value);
+                    });
+                  },
+                  imagePath: ("assets/euro/10.png"),
                 ),
               ],
             ),
@@ -109,35 +151,25 @@ class _TapTapCounterScreenState extends State<TapTapCounterScreen> {
           Expanded(
             child: Row(
               children: [
-                Expanded(
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    child: Image.asset("assets/euro/20.jpg"),
-                  ),
+                MoneyButton(
+                  value: 20,
+                  onPressed: (value) {
+                    setState(() {
+                      moneyCounter += value;
+                      moneySequence.add(value);
+                    });
+                  },
+                  imagePath: ("assets/euro/20.jpg"),
                 ),
-                Expanded(
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    child: Image.asset("assets/euro/50.jpeg"),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Expanded(
-            child: Row(
-              children: [
-                Expanded(
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    child: Image.asset("assets/euro/100.jpeg"),
-                  ),
-                ),
-                Expanded(
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    child: Image.asset("assets/euro/200.png"),
-                  ),
+                MoneyButton(
+                  value: 50,
+                  onPressed: (value) {
+                    setState(() {
+                      moneyCounter += value;
+                      moneySequence.add(value);
+                    });
+                  },
+                  imagePath: ("assets/euro/50.jpeg"),
                 ),
               ],
             ),
@@ -145,15 +177,49 @@ class _TapTapCounterScreenState extends State<TapTapCounterScreen> {
           Expanded(
             child: Row(
               children: [
-                Expanded(
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    child: Image.asset("assets/euro/500.png"),
-                  ),
+                MoneyButton(
+                  value: 100,
+                  onPressed: (value) {
+                    setState(() {
+                      moneyCounter += value;
+                      moneySequence.add(value);
+                    });
+                  },
+                  imagePath: ("assets/euro/100.jpeg"),
+                ),
+                MoneyButton(
+                  value: 200,
+                  onPressed: (value) {
+                    setState(() {
+                      moneyCounter += value;
+                      moneySequence.add(value);
+                    });
+                  },
+                  imagePath: ("assets/euro/200.png"),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                MoneyButton(
+                  value: 500,
+                  onPressed: (value) {
+                    setState(() {
+                      moneyCounter += value;
+                      moneySequence.add(value);
+                    });
+                  },
+                  imagePath: ("assets/euro/500.png"),
                 ),
                 Expanded(
                   child: ElevatedButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      setState(() {
+                        moneyCounter -= moneySequence.removeLast();
+                      });
+                    },
                     child: Icon(Icons.undo),
                   ),
                 ),
@@ -162,6 +228,7 @@ class _TapTapCounterScreenState extends State<TapTapCounterScreen> {
           ),
         ],
       ),
+      bottomNavigationBar: Text("€${moneyCounter.toStringAsFixed(2)}"),
     );
   }
 }
